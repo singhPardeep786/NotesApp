4956ccd2f5eb1f17961b3e5f760f01224a0ef898
singhPardeep786 <pardeep90191170@gmail.com>
2026-09-15 20:28:32 +0530

    data-render-fixed

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const noteModel = require('./models/notes.model.js')
+const noteModel = require('./models/notes.model')
 const cors = require('cors')
 const path = require('path')
-
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('/public'))
+app.use(express.static('./public'))
 
 // POST /api/notes
 app.post('/api/notes', async (req, res) => {
